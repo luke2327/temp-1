6b6ad9270e907c0c6b0345a1a8dd9c4e5b82607a
loud <78677354+jooy0510@users.noreply.github.com>
2023-09-16 00:51:58 +0900

    Update PhotosList.tsx

--- src/app/photos/PhotosList.tsx
+++ src/app/photos/PhotosList.tsx
@@ -13,7 +13,7 @@ interface Props {
 export default function PhotosList({ list }: Props) {
   return (
     <div className="md:flex md:gap-4 md:flex-wrap md:flex-grow-0 md:flex-shrink-0 justify-center max-w-[78rem]">
-      {list.map(({ id, cover, properties }, idx) => (
+      {list?.map(({ id, cover, properties }, idx) => (
         <Card key={id} className="w-[18rem] min-h-[18rem] my-4">
           <CardBody className="w-full h-full flex justify-center items-center">
             {cover == null ? (
@@ -24,17 +24,17 @@ export default function PhotosList({ list }: Props) {
           </CardBody>
           <CardHeader className="w-full flex flex-col justify-center align-center">
             <span className="font-bold ">
-              {properties.곡명.title.map((title) => title.plain_text)}
+              {properties.곡명.title?.map((title) => title.plain_text)}
             </span>
             <div className={styles.practiceStatus}>
-              {properties.상태.multi_select.map((status: any) => (
+              {properties.상태.multi_select?.map((status: any) => (
                 <Button size="sm" radius="full" key={status.id} className="">
                   {status.name}
                 </Button>
               ))}
             </div>
             <span className="max-w-[18rem] text-sm opacity-50">
-              {properties['인원, 구성'].rich_text.map(
+              {properties['인원, 구성'].rich_text?.map(
                 (rich_text) => rich_text.plain_text
               )}
             </span>
@@ -50,7 +50,7 @@ function NotionImage({ cover }: { cover: Cover }) {
 
   return (
     <>
-      {keys.map((key, idx) => (
+      {keys?.map((key, idx) => (
         <Image key={idx} src={cover[key].url} alt="연습 사진" fill></Image>
       ))}
     </>
