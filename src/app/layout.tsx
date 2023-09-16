93699a8ff9de8c0c5beec55e6462927290a07cc1
loud <78677354+jooy0510@users.noreply.github.com>
2023-09-17 00:40:21 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children, }: { children: React.ReactNode ) {
+export default function RootLayout({ children, }: { children: React.ReactNode; ) {
   return (
     <html>
       <body className="w-screen">
