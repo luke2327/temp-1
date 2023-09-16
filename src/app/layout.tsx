a559e7ef64e904ffd4f3dd6de67b75f2fe939a68
loud <78677354+jooy0510@users.noreply.github.com>
2023-09-16 22:49:16 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ 
   children, 
 }: {
-  children: React.ReactNode;
+  children: PropsWithChildren;
 }) {
   return (
     <html>
